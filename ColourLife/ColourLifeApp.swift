01f1d903762a066deffe46cbfbd6af8785887dc7
Yuruno <82246763+Yuruno@users.noreply.github.com>
2021-11-19 22:46:04 +0800

    Initial Commit

---
+++ ColourLife/ColourLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColourLifeApp.swift
+//  ColourLife
+//
+//  Created by sap on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ColourLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
