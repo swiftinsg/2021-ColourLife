37a763dea2ba65b69129ef4f4acf5b5a31d20165
Yuruno <82246763+Yuruno@users.noreply.github.com>
2021-11-24 00:32:57 +0800

    Filter testing, slider disappears when "Normal" is chosen

--- ColourLife/ContentView.swift
+++ ColourLife/ContentView.swift
@@ -1,45 +1,96 @@
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @StateObject private var model = ContentViewModel()
     @State private var severity: Double = 0
-//    @State var toggle1: Bool = true
-//    @State var toggle2: Bool = false
-//    @State var toggle3: Bool = false
-//    @State var toggle4: Bool = false
-//    @State var toggle5: Bool = false
+    //    @State var toggle1: Bool = true
+    //    @State var toggle2: Bool = false
+    //    @State var toggle3: Bool = false
+    //    @State var toggle4: Bool = false
+    //    @State var toggle5: Bool = false
     let types = ["Normal", "Deuteranopia","Protanopia","Tritanopia","Achromatopsia"]
     @State var buttonPressed = 0
     
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    @State var image = Image("apples-2")
+    @State var inputImage = UIImage(imageLiteralResourceName: "apples-2")
+    
+    func applyFilter() {
+        switch buttonPressed {
+        case 0:
+            image = Image(uiImage: inputImage)
+        default:
+            loadImage()
+        }
+    }
+    
+    func applyProcessing() {
+        
+        currentFilter.intensity = Float(severity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
+    }
+    
+    func loadImage() {
+        //        guard let inputImage = inputImage else { return }
+        
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
     var body: some View {
-        ZStack {
-            
-            ZStack {
-                //            FrameView(image: model.frame)
-                //                .edgesIgnoringSafeArea(.all)
-                //            ErrorView(error: model.error)
-                Image("apples-2")
-                    .resizable()
-                    .edgesIgnoringSafeArea(.all)
+        let intensity = Binding<Double>(
+            get: {
+                self.severity
+            },
+            set: {
+                self.severity = $0
+                self.applyProcessing()
             }
+        )
+        ZStack(alignment: .bottom) {
             
-            ZStack {
+            //            FrameView(image: model.frame)
+            //                .edgesIgnoringSafeArea(.all)
+            //            ErrorView(error: model.error)
+            //                Image("apples-2")
+            image
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            
+            ZStack(alignment: .bottom) {
                 
                 Color.white
                 
-                VStack{
-                    HStack(alignment: .center){
-                        Text("Mild")
-                            .padding()
-                        Slider(value: $severity, in: 0...20)
-                        Text("Severe")
-                            .padding()
+                VStack {
+                    if (buttonPressed != 0) {
+                        HStack(alignment: .center){
+                            Text("Mild")
+                                .padding()
+                            Slider(value: intensity, in: 0...20) {_ in
+                                applyProcessing()
+                            }
+                            Text("Severe")
+                                .padding()
+                        }
                     }
                     ScrollView(.horizontal){
                         HStack{
                             ForEach(0..<types.count) {type in
                                 Button(types[type]) {
                                     buttonPressed = type
+                                    applyFilter()
                                     //filterFunction(type)
                                 }
                                 .frame(height: 30)
@@ -47,81 +98,11 @@ struct ContentView: View {
                                 .background(buttonPressed==type ? Color.blue : Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.3))
                                 .foregroundColor(buttonPressed==type ? Color.white : Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.5))
                                 .cornerRadius(15)
-
+                                
                                 
                             }
-                            Spacer(minLength: 30)
-//                            Button("Normal"){
-//                                //no filter
-//                                toggle1 = true
-//                                toggle2 = false
-//                                toggle3 = false
-//                                toggle4 = false
-//                                toggle5 = false
-//                            }
-//                            .frame(height: 30)
-//                            .padding(10)
-//                            .background(toggle1 ? Color.blue : Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.3))
-//                            .foregroundColor(toggle1 ? Color.white : Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.5))
-//                            .cornerRadius(15)
-//
-//                            Button("Deutanopia"){
-//                                //Deutanopia filter
-//                                toggle1 = false
-//                                toggle2 = true
-//                                toggle3 = false
-//                                toggle4 = false
-//                                toggle5 = false
-//                            }
-//                            .frame(height: 30)
-//                            .padding(10)
-//                            .background(toggle2 ? Color.blue : Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.3))
-//                            .foregroundColor(toggle2 ? Color.white : Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.5))
-//                            .cornerRadius(15)
-//
-//                            Button("Protanopia"){
-//                                //Protanopia filter
-//                                toggle1 = false
-//                                toggle2 = false
-//                                toggle3 = true
-//                                toggle4 = false
-//                                toggle5 = false
-//                            }
-//                            .frame(height: 30)
-//                            .padding(10)
-//                            .background(toggle3 ? Color.blue : Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.3))
-//                            .foregroundColor(toggle3 ? Color.white : Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.5))
-//                            .cornerRadius(15)
-//
-//                            Button("Tritanopia"){
-//                                //Tritanopia filter
-//                                toggle1 = false
-//                                toggle2 = false
-//                                toggle3 = false
-//                                toggle4 = true
-//                                toggle5 = false
-//                            }
-//                            .frame(height: 30)
-//                            .padding(10)
-//                            .background(toggle4 ? Color.blue : Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.3))
-//                            .foregroundColor(toggle4 ? Color.white : Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.5))
-//                            .cornerRadius(15)
-//
-//                            Button("Achromatopsia"){
-//                                //Achromatopsia filter
-//                                toggle1 = false
-//                                toggle2 = false
-//                                toggle3 = false
-//                                toggle4 = false
-//                                toggle5 = true
-//                            }
-//                            .frame(height: 30)
-//                            .padding(10)
-//                            .background(toggle5 ? Color.blue : Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.3))
-//                            .foregroundColor(toggle5 ? Color.white : Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.5))
-//                            .cornerRadius(15)
                         }
-                        .offset(x:15)
+                        .padding(.horizontal,10)
                     }
                     
                     HStack{
@@ -146,9 +127,9 @@ struct ContentView: View {
                     .offset(y: -20)
                 }
             }
-            .frame(height: 220)
-            .offset(y: 310)
+            .frame(height: buttonPressed == 0 ? 175:220)
         }
+        .frame(height: UIScreen.main.bounds.size.height)
     }
 }
 
@@ -156,6 +137,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(image: Image("apples-1"))
     }
 }
