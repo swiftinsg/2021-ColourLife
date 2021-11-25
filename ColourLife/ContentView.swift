78ed449737956b00a5fd5984f30d2e45c81faf97
Matthew Christopher Tan Ming Wen <mctmw2006@gmail.com>
2021-11-25 10:17:29 +0800

    Added info sheets and fixed UI

--- ColourLife/ContentView.swift
+++ ColourLife/ContentView.swift
@@ -5,103 +5,85 @@ import CoreImage.CIFilterBuiltins
 struct ContentView: View {
     @StateObject private var model = ContentViewModel()
     @State private var severity: Double = 0
-    //    @State var toggle1: Bool = true
-    //    @State var toggle2: Bool = false
-    //    @State var toggle3: Bool = false
-    //    @State var toggle4: Bool = false
-    //    @State var toggle5: Bool = false
+    @State private var infoViewIsPresented = false
+    @State private var pictureViewIsPresented = false
+    
     let types = ["Normal", "Deuteranopia","Protanopia","Tritanopia","Achromatopsia"]
     @State var buttonPressed = 0
     
     @State private var currentFilter = CIFilter.sepiaTone()
     let context = CIContext()
-    //    @State var image = UIImage(imageLiteralResourceName: "apples-2")
-    //    @State var inputImage: CGImage?
+    @State var image = Image("apples-2")
+    @State var inputImage = UIImage(imageLiteralResourceName: "apples-2")
     
-    func applyFilter() -> CGImage? {
-        let ciContext = CIContext()
-                    let myImage = ciContext.createCGImage(CIImage(image: UIImage(imageLiteralResourceName: "grocery_store"))!, from: CIImage(image: UIImage(imageLiteralResourceName: "grocery_store"))!.extent)!
+    let info = [
+        "It is estimated that a person with normal color vision can see up to 1 million distinct shades of color, but a person who is color blind may see as few as just 10 thousand colors (1% of the normal range)",
+        "A person with deutan color vision deficiency may experience confusions between colors such as green and yellow, or blue and purple. Another common symptom is that green traffic signals appear to be a very pale green or sometimes white. Common color confusion also occurs between pink and gray or white, especially if the pink is similar to a light purple.",
+        "A person with protan type color blindness tends to see greens, yellows, oranges, reds, and browns as being more similar shades of color than normal, especially in low light. A very common problem is that purple colors look more like blue. Another common issue is that pink colors appear to be gray, especially if the pink is a more reddish pink or salmon color. Another symptom specific to protan color vision deficiency is that red colors look darker than normal. For example, if red text is printed on a black background, it can be very hard to read because the red appears to be very dark.",
+        "Typically a person with a tritan-type color vision deficiency does not see blue colors well, and may have difficulty seeing the difference between blue and green. Cataracts, glaucoma, and age-related macular degeneration can cause symptoms of tritan color blindness. Another factor that causes reduced sensitivity to blue is the yellowing of the crystalline lens within the eye: these cells do not regenerate and over a lifetime of exposure to light, especially UV light, the lens tends to become yellow in appearance and block the transmission of blue light, interfering with color vision. Eventually this yellowing also leads to cataracts that must be treated surgically.",
+        "Similar to other forms of color blindness, achromatopsia can be graded as incomplete (partial) achromatopsia or complete achromatopsia (total color blindness). Achromatopsia is often associated with light sensitivity, photophobia, and glare sensitivity. In some cases, low vision disorders such as progressive cone dystrophy or retinitis pigmentosa can cause a gradual deterioration of color vision that eventually turns into complete achromatopsia."
+    ]
+    let links = [
+        Link("More about Normal Vision",
+             destination: URL(string: "https://enchroma.com/blogs/beyond-color/how-color-blind-see")!),
+        Link("More about Deuteranopia",
+             destination: URL(string: "https://enchroma.com/pages/deutan")!),
+        Link("More about Protanopia",
+             destination: URL(string: "https://enchroma.com/pages/protan")!),
+        Link("More about Tritanopia",
+             destination: URL(string: "https://enchroma.com/blogs/beyond-color/how-color-blind-see")!),
+        Link("More about Achromatopsia",
+             destination: URL(string: "https://enchroma.com/blogs/beyond-color/how-color-blind-see")!)
+    ]
+    
+    func applyFilter() {
         switch buttonPressed {
         case 0:
-            //            image = UIImage(model.frame)
-            return model.frame
-        case 1:
-            return applyDeutanFilter(input: myImage)
-//            currentFilter = DeutanFilter()
-        case 2:
-            return applyProtanFilter(input: myImage)
-//            currentFilter = ProtanFilter()
-        case 3:
-            return applyTritanFilter(input: myImage)
-//            currentFilter = TritanFilter()
+            image = Image(uiImage: inputImage)
         default:
-            return loadImage()
+            loadImage()
         }
     }
     
-    func applyProtanFilter(input: CGImage) -> CGImage {
-        let filter = ProtanFilter()
-        let beginImage =  CIImage(cgImage: input)
-        filter.inputImage = beginImage
-        filter.severity = Float(severity)
-        if let outputImage = filter.outputImage {
-            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-                return cgimg
-            }
-        }
-        return input
-    }
-    func applyDeutanFilter(input: CGImage) -> CGImage {
-        let filter = DeutanFilter()
-        let beginImage =  CIImage(cgImage: input)
-        filter.inputImage = beginImage
-        filter.severity = Float(severity)
-        if let outputImage = filter.outputImage {
-            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-                return cgimg
-            }
-        }
-        return input
-    }
-    func applyTritanFilter(input: CGImage) -> CGImage {
-        let filter = TritanFilter()
-        let beginImage =  CIImage(cgImage: input)
-        filter.inputImage = beginImage
-        filter.severity = Float(severity)
-        if let outputImage = filter.outputImage {
-            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-                return cgimg
-            }
-        }
-        return input
-    }
-    func loadImage() -> CGImage? {
-        guard let inputImage = model.frame else { return model.frame}
-        
-        let beginImage =  CIImage(cgImage: inputImage)
-        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+    func applyProcessing() {
         
         currentFilter.intensity = Float(severity)
         
-        guard let outputImage = currentFilter.outputImage else { return model.frame}
+        guard let outputImage = currentFilter.outputImage else { return }
         
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-            //            let uiImage = UIImage(cgImage: cgimg)
-            return cgimg
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
         }
-        return model.frame
+    }
+    
+    func loadImage() {
+        //        guard let inputImage = inputImage else { return }
+        
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
     
     var body: some View {
+        let intensity = Binding<Double>(
+            get: {
+                self.severity
+            },
+            set: {
+                self.severity = $0
+                self.applyProcessing()
+            }
+        )
         ZStack(alignment: .bottom) {
             
-            FrameView(image:  applyFilter())
-                .edgesIgnoringSafeArea(.all)
-            //                        ErrorView(error: model.error)
-            //                            Image("apples-2")
-            //            image
-            //                .resizable()
+            //            FrameView(image: model.frame)
             //                .edgesIgnoringSafeArea(.all)
+            //            ErrorView(error: model.error)
+            //                Image("apples-2")
+            image
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
             
             
             
@@ -114,14 +96,9 @@ struct ContentView: View {
                         HStack(alignment: .center){
                             Text("Mild")
                                 .padding()
-                            Slider(value: $severity, in: 1...10,step:1)
-//                            { _ in
-//                                let ciContext = CIContext()
-//                                let myImage = ciContext.createCGImage(CIImage(image: UIImage(imageLiteralResourceName: "apples-2"))!, from: CIImage(image: UIImage(imageLiteralResourceName: "apples-2"))!.extent)!
-//
-//                                FrameView(image:applyColorKernal(input: myImage))
-//
-//                            }
+                            Slider(value: intensity, in: 0...20) {_ in
+                                applyProcessing()
+                            }
                             Text("Severe")
                                 .padding()
                         }
@@ -131,6 +108,8 @@ struct ContentView: View {
                             ForEach(0..<types.count) {type in
                                 Button(types[type]) {
                                     buttonPressed = type
+                                    applyFilter()
+                                    //filterFunction(type)
                                 }
                                 .frame(height: 30)
                                 .padding(10)
@@ -146,22 +125,58 @@ struct ContentView: View {
                     
                     HStack{
                         Button("\(Image(systemName: "questionmark.circle.fill"))"){
-                            //info sheet
+                            infoViewIsPresented = true
+                        }
+                        .sheet(isPresented: $infoViewIsPresented) {
+                            ScrollView{
+                                Text("\(types[buttonPressed])")
+                                    .font(.system(.largeTitle))
+                                    .fontWeight(.bold)
+                                VStack {
+                                    Text("\(info[buttonPressed])")
+                                        .font(.system(size:25))
+                                        .padding(20)
+                                    links[buttonPressed]
+                                        .padding()
+                                }
+                            }
                         }
                         .font(.system(size: 30))
-                        .offset(x: -90)
+                        .offset(x: -UIScreen.main.bounds.size.width/6)
                         
                         Button("\(Image(systemName: "camera.circle.fill"))"){
-                            //picture sheet
+                            pictureViewIsPresented = true
                         }
                         .font(.system(size: 70))
                         .foregroundColor(Color.black)
+                        .fullScreenCover(isPresented: $pictureViewIsPresented) {
+                            //InfoView()
+                            ZStack {
+                                Text("\(Image("apples-1"))")
+                                VStack {
+                                    Button("Save to Photos"){
+                                        //request access to photos
+                                    }
+                                    .frame(height: 30)
+                                    .padding(10)
+                                    .background(Color.blue)
+                                    .foregroundColor(Color.white)
+                                    .cornerRadius(15)
+                                    Button("Dismiss"){
+                                        pictureViewIsPresented = false
+                                    }
+                                    .foregroundColor(Color.red)
+                                }
+                                .offset(y: UIScreen.main.bounds.size.height/2.5)
+                            }
+                        }
                         
                         Button("\(Image(systemName: "photo.fill.on.rectangle.fill"))"){
                             //photos app
+                            //request permission
                         }
                         .font(.system(size: 30))
-                        .offset(x: 90)
+                        .offset(x: UIScreen.main.bounds.size.width/6)
                     }
                     .offset(y: -20)
                 }
@@ -176,6 +191,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(image: Image("apples-2"))
     }
 }
