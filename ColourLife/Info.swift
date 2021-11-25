8f43b4b2f81064b73357665a18c6bedbcf67175d
Yuruno <82246763+Yuruno@users.noreply.github.com>
2021-11-25 13:28:21 +0800

    Moved info to its own file

---
+++ ColourLife/Info.swift
@@ -0,0 +1,30 @@
+//
+//  Info.swift
+//  ColourLife
+//
+//  Created by sap on 25/11/21.
+//
+
+import Foundation
+import SwiftUI
+
+
+let info = [
+    "It is estimated that a person with normal color vision can see up to 1 million distinct shades of color, but a person who is color blind may see as few as just 10 thousand colors (1% of the normal range)",
+    "A person with deutan color vision deficiency may experience confusions between colors such as green and yellow, or blue and purple. Another common symptom is that green traffic signals appear to be a very pale green or sometimes white. Common color confusion also occurs between pink and gray or white, especially if the pink is similar to a light purple.",
+    "A person with protan type color blindness tends to see greens, yellows, oranges, reds, and browns as being more similar shades of color than normal, especially in low light. A very common problem is that purple colors look more like blue. Another common issue is that pink colors appear to be gray, especially if the pink is a more reddish pink or salmon color. Another symptom specific to protan color vision deficiency is that red colors look darker than normal. For example, if red text is printed on a black background, it can be very hard to read because the red appears to be very dark.",
+    "Typically a person with a tritan-type color vision deficiency does not see blue colors well, and may have difficulty seeing the difference between blue and green. Cataracts, glaucoma, and age-related macular degeneration can cause symptoms of tritan color blindness. Another factor that causes reduced sensitivity to blue is the yellowing of the crystalline lens within the eye: these cells do not regenerate and over a lifetime of exposure to light, especially UV light, the lens tends to become yellow in appearance and block the transmission of blue light, interfering with color vision. Eventually this yellowing also leads to cataracts that must be treated surgically.",
+    "Similar to other forms of color blindness, achromatopsia can be graded as incomplete (partial) achromatopsia or complete achromatopsia (total color blindness). Achromatopsia is often associated with light sensitivity, photophobia, and glare sensitivity. In some cases, low vision disorders such as progressive cone dystrophy or retinitis pigmentosa can cause a gradual deterioration of color vision that eventually turns into complete achromatopsia."
+]
+let links = [
+    Link("More about Normal Vision",
+         destination: URL(string: "https://enchroma.com/blogs/beyond-color/how-color-blind-see")!),
+    Link("More about Deuteranopia",
+         destination: URL(string: "https://enchroma.com/pages/deutan")!),
+    Link("More about Protanopia",
+         destination: URL(string: "https://enchroma.com/pages/protan")!),
+    Link("More about Tritanopia",
+         destination: URL(string: "https://enchroma.com/blogs/beyond-color/how-color-blind-see")!),
+    Link("More about Achromatopsia",
+         destination: URL(string: "https://enchroma.com/blogs/beyond-color/how-color-blind-see")!)
+]
